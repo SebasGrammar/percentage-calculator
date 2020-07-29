51a583b3115f17f60405e46c53c0210e6b810495
Sebastian <sebasgrammar@hotmail.com>
2020-07-28 22:18:53 -0500

    made first calculator. 7 more to go

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const amount = document.querySelector(".amount"),
-    percent = document.querySelector(".percent");
+    percent = document.querySelector(".percent"),
+    base = document.querySelector(".base");
 
 const bar = document.querySelector(".bar");
 const progress = bar.querySelector(".progress");
@@ -8,14 +9,10 @@ function updateVariable() {
 
     const total = 100 - (100 - percent.value)
 
-    const suffix = this.dataset.sizing || '';
-    // document.documentElement.style.setProperty(`--${this.dataset.name}`, this.value + suffix);
-    document.documentElement.style.setProperty(`--${this.dataset.name}`, total + suffix);
+    document.documentElement.style.setProperty(`--${this.dataset.name}`, `${total}%`);
 
-    console.log(this.dataset.name)
-    console.log(percent.value + suffix)
+    console.log(Number(percent.value) * Number(base.value) / 100)
+    amount.textContent = Number(percent.value) * Number(base.value) / 100
 }
-
-//progress.addEventListener("change", updateVariable)
 
 percent.addEventListener("input", updateVariable.bind(progress))
--- src/js/bar.js
+++ src/js/bar.js
@@ -1,16 +1,33 @@
 const amount = document.querySelector(".amount"),
-    percent = document.querySelector(".percent");
+    percent = document.querySelector(".percent"),
+    base = document.querySelector(".base");
 
 const bar = document.querySelector(".bar");
 const progress = bar.querySelector(".progress");
 
+// function updateVariable() {
+
+//     const total = 100 - (100 - percent.value)
+
+//     const suffix = this.dataset.sizing || '';
+//     // document.documentElement.style.setProperty(`--${this.dataset.name}`, this.value + suffix);
+//     document.documentElement.style.setProperty(`--${this.dataset.name}`, total + suffix);
+
+//     console.log(this.dataset.name)
+//     console.log(percent.value + suffix)
+
+
+//     console.log(Number(percent.value) * Number(base.value) / 100)
+// }
+
 function updateVariable() {
-    const suffix = this.dataset.sizing || '';
-    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
 
-    console.log(this)
+    const total = 100 - (100 - percent.value)
+
+    document.documentElement.style.setProperty(`--${this.dataset.name}`, `${total}%`);
+
+    console.log(Number(percent.value) * Number(base.value) / 100)
+    amount.textContent = Number(percent.value) * Number(base.value) / 100
 }
-
-//progress.addEventListener("change", updateVariable)
 
 percent.addEventListener("input", updateVariable.bind(progress))
