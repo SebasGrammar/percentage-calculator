beefa9d032113c74daf6cc13f3ea8a8adb2d08ef
Sebastian <sebasgrammar@hotmail.com>
2020-08-13 22:50:46 -0500

    found a way to add the extra fee when price is below 70000

--- tests/main.js
+++ tests/main.js
@@ -7,7 +7,8 @@ const calculator = document.querySelector(".calculator");
 const salePrice = calculator.querySelector(".sale-price"), // Selling price -> all units + commission + shipping
     profits = calculator.querySelector(".profit"), // profit after selling specified units
     unitPrice = calculator.querySelector(".unit-price"),
-    commissionValue = calculator.querySelector(".commission-value"); // unit price after applying commission
+    commissionValue = calculator.querySelector(".commission-value"),
+    unitWithoutCommission = calculator.querySelector(".unit-without-commission"); // unit price after applying commission
 
 // DATA
 
@@ -40,8 +41,10 @@ function calculate() {
 
     // Price after applying selling fees (commission)
     // let unitSellingPrice = Math.round((price + profit) / (1 - commissionFee / 100))
-    let unitSellingPrice = (price + profit) / (1 - commissionFee / 100)
+    let unitSellingPrice = (price + profit) / (1 - commissionFee / 100);
 
+    // Price without commission
+    let priceWithoutCommission = price + profit;
 
     /* TEST */
 
@@ -50,18 +53,37 @@ function calculate() {
 
     let shipp = y / x
 
+    /* TEST ABOVE */
+
+    /* TEST */
+
+    let z = numberOfUnits * 500
+    let ext = z / x
+
+    /* TEST ABOVE */
+
+    //let w = numberOfUnits * ()
+
     // CONTENT
     // salePrice.textContent = Math.round( unitSellingPrice * numberOfUnits + shippingFee + (19.05 * shippingFee / 100) );
-    let w = Math.round( unitSellingPrice * numberOfUnits + shipp);
+    // salePrice.textContent = Math.round( unitSellingPrice * numberOfUnits + shipp);
+    
+    //let w = Math.round( unitSellingPrice * numberOfUnits + shipp);
+    let w = Math.round( unitSellingPrice * numberOfUnits + shipp + (numberOfUnits * 500));
+
     
     if (w < 70000) {
     //  salePrice.textContent = Math.round( unitSellingPrice * numberOfUnits + shipp);
-        "SMALler"
-        //w += 500
-        commissionValue.textContent = Math.round( commissionFee * Number(salePrice.textContent) / 100) + 500;
+        // console.log("SMALler")
+        // w += 500
+        // commissionValue.textContent = Math.round( commissionFee * Number(salePrice.textContent) / 100) + 500;
+
+        salePrice.textContent = Math.round( unitSellingPrice * numberOfUnits + shipp + ext);
+
+        // THERE IS A DIFFERENCE OF 500!!!!
     } 
     // salePrice.textContent = Math.round( unitSellingPrice * numberOfUnits + shipp);
-    salePrice.textContent = w + 500;
+    //salePrice.textContent = w;
 
     /* TEST */
 
@@ -81,7 +103,8 @@ function calculate() {
     // salePrice.textContent = Math.round( unitSellingPrice * numberOfUnits + shipp );
     profits.textContent = profit * numberOfUnits;
     unitPrice.textContent = Math.round(unitSellingPrice);
-    // commissionValue.textContent = Math.round( commissionFee * Number(salePrice.textContent) / 100);
+    commissionValue.textContent = Math.round( commissionFee * Number(salePrice.textContent) / 100);
+    unitWithoutCommission.textContent = priceWithoutCommission
     
 
 
